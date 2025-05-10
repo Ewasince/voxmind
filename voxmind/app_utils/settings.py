c73ae3eb46b897abcf093abb9fcb7e9f16dd37ff
Vladislav Lavrenov <ewasince@gmail.com>
2025-05-10 16:12:06 +0400

    ref! change key_phase

--- voxmind/app_utils/settings.py
+++ voxmind/app_utils/settings.py
@@ -7,6 +7,6 @@ class Settings(BaseSettings):
     gigachat_token: str | None = Field(default=None)
 
     # recognizing settings
-    key_phase: str = Field(default="jarvis")
+    key_phase: str = Field(default="помощник")
     regexp: str = Field(default=r"[^A-Za-zА-ЯЁа-яё0-9 ]")
     language: str = "ru-RU"
